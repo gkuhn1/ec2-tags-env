b65da5017a9f3f613b14e1707403a7fc17df1f50
Guilherme Kuhn <g.kuhn0@gmail.com>
2015-08-18 12:40:54 -0300

    set to ignore : in tags eg: AWS:AUTOSCALING:GROUPNAME=XXXXXXXX

--- import-tags.sh
+++ import-tags.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-get_ami_tags () {
+get_instance_tags () {
     instance_id=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/instance-id)
     echo $(/usr/local/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id")
 }
 
-get_instance_tags () {
+get_ami_tags () {
     ami_id=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/ami-id)
     echo $(/usr/local/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$ami_id")
 }
@@ -16,6 +16,8 @@ tags_to_env () {
     for key in $(echo $tags | /usr/bin/jq -r ".[][].Key"); do
         value=$(echo $tags | /usr/bin/jq -r ".[][] | select(.Key==\"$key\") | .Value")
         key=$(echo $key | /usr/bin/tr '-' '_' | /usr/bin/tr '[:lower:]' '[:upper:]')
+        key=$(sed "s/://g" <<< $key)
+        echo "$key=$value"
         export $key=$value
     done
 }
